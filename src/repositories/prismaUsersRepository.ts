5d0976f33af53a8c9cb235cb56078f2b45045691
assef <andreassef02@gmail.com>
2024-04-23 22:03:56 -0300

    users repository

---
+++ src/repositories/prismaUsersRepository.ts
@@ -0,0 +1,25 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { UsersRepositoryInterface } from './usesRepositoryInterface'
+
+class PrismaUsersRepository implements UsersRepositoryInterface {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
+}
+
+export { PrismaUsersRepository }
