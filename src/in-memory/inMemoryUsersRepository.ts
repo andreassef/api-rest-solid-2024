36d2d216e2f703f181c9b428ec54accad0738f45
assef <andreassef02@gmail.com>
2024-05-01 18:30:08 -0300

    new in memory use cases

--- src/in-memory/inMemoryUsersRepository.ts
+++ src/in-memory/inMemoryUsersRepository.ts
@@ -1,8 +1,19 @@
-import { UsersRepositoryInterface } from '@/repositories/usesRepositoryInterface'
+import { UsersRepository } from '@/repositories/usesRepository'
 import { Prisma, User } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
 
-class InMemoryUsersRepository implements UsersRepositoryInterface {
+class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
 
   async findByEmail(email: string) {
     const user = this.items.find((item) => item.email === email)
@@ -16,7 +27,7 @@ class InMemoryUsersRepository implements UsersRepositoryInterface {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
