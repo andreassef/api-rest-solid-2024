4cfe68661aabdfc261b733ed80bcd73599180a83
assef <andreassef02@gmail.com>
2024-04-30 22:21:30 -0300

    creating factories

---
+++ src/use-cases/factories/makeAuthenticateUseCase.ts
@@ -0,0 +1,9 @@
+import { PrismaUsersRepository } from '@/repositories/prismaUsersRepository'
+import { AuthenticateUseCase } from '../authenticate'
+
+export function makeAuthenticateUseCase() {
+  const usersRepository = new PrismaUsersRepository()
+  const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+  return authenticateUseCase
+}
