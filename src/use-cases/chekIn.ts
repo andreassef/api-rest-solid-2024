d93c553060f4cf094db98fa402a1f0f4f97653e8
assef <andreassef02@gmail.com>
2024-05-02 19:10:09 -0300

    adding date validation on checkIn use case

--- src/use-cases/chekIn.ts
+++ src/use-cases/chekIn.ts
@@ -17,6 +17,15 @@ class CheckinUseCase {
     userId,
     gymId,
   }: CheckinUseCaseRequest): Promise<CheckinUseCaseResponse> {
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date(),
+    )
+
+    if (checkInOnSameDay) {
+      throw new Error('Checkin Already exists')
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
