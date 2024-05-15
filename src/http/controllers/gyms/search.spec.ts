3980da4ad6d12f4515b2b0215ddc731d8fb4f370
assef <andreassef02@gmail.com>
2024-05-15 18:46:08 -0300

    e2e tests to gyms

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,43 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/tests/createAndAuthenticateUser'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search a gym by title', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Javascript Gym',
+        description: 'Some cool description',
+        phone: '51999999999',
+        latitude: -29.9499867,
+        longitude: -51.0929696,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({ q: 'Javascript' })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Javascript Gym',
+      }),
+    ])
+  })
+})
